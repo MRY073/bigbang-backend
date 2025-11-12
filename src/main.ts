85732318fa7314442b5a5e46e94fae7134069d33
MRY073 <yifuturetelling857@gmail.com>
2025-11-13 01:04:20 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser()); // <-- 启用 cookie 解析
-
   // 根据环境变量设置全局前缀
   const isProduction = process.env.NODE_ENV === 'production';
   if (isProduction) {
